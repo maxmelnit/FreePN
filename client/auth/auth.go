36fa4a15198fab520febbcc3d643c109cc7b05da
JryMx <65923935+JryMx@users.noreply.github.com>
2026-06-21 13:44:34 -0600

    Implemented client-side AES-GCM packet encryption/decryption

--- client/auth/auth.go
+++ client/auth/auth.go
@@ -1,2 +1,62 @@
 package auth
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"log"
+)
+
+func Encrypt(key []byte, data []byte) ([]byte, error) {
+
+	// Make a unique cipher block
+	block, err := aes.NewCipher(key)
+
+	if err != nil {
+		log.Println("Could not initialize cipher block: " + err.Error())
+		return nil, err
+	}
+
+	// GCM: Make into stream cipher + add tamper prevention (tag and nonce)
+	gcm, err := cipher.NewGCMWithRandomNonce(block)
+
+	if err != nil {
+		log.Println("Could not wrap AES in GCM" + err.Error())
+		return nil, err
+	}
+
+	// Encrypt and return the data
+	encrypted := gcm.Seal(nil, nil, data, nil)
+
+	return encrypted, nil
+
+}
+
+func Decrypt(key []byte, data []byte) ([]byte, error) {
+
+	block, err := aes.NewCipher(key)
+
+	if err != nil {
+		log.Println("Could not initialize cipher block: " + err.Error())
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCMWithRandomNonce(block)
+
+	if err != nil {
+		log.Println("Could not wrap AES in GCM" + err.Error())
+		return nil, err
+	}
+
+	res, err := gcm.Open(nil, nil, data, nil)
+
+	if err != nil {
+		log.Println("Error decrypting data" + err.Error())
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func KeyExchange() {
+	// TODO
+}
